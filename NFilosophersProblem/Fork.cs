18010f43427efa1f2e9cf715cf49f6d5a24842f3
Marcin Matysiak <256850@student.pwr.edu.pl>
2023-04-28 00:14:26 +0200

    Commit1

--- NFilosophersProblem/Fork.cs
+++ NFilosophersProblem/Fork.cs
@@ -3,17 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+
 
 namespace NFilosophersProblem
 {
     internal class Fork
     {
+        LinkedList<Philosopher> circle = new LinkedList<Philosopher>();
         bool isPicked = false; //na poczatku polozony
 
-        public void acquire()
+        public void start(int n)
+        {
+            Thread[] threads = new Thread[n];
+            for (int i = 0; i < n; i++)
+            {
+                threads[i] = new Thread(new ParameterizedThreadStart(think_eat));
+                threads[i].Start(i);
+            }
+        }
+        public void think_eat(object id)
+        {
+            int philosopherId = (int)id;
+            circle.AddLast(new Philosopher { ID = philosopherId});
+        }
+        public void acquireleft()
         {
 
-        }   
+        }
+
+        public void acquireright()
+        {
+
+        }
 
         public void release()
         {
@@ -22,12 +44,11 @@ namespace NFilosophersProblem
 
     }
 
-    internal class Philosopher { 
-        int ID { get; set; }
+    public class Philosopher { 
+        public int ID { get; set; }
         bool leftFork = false;  
         bool rightFork = false;
         bool forks = false;
-    
     }
 
 }
