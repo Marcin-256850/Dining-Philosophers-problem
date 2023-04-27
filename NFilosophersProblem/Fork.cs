d093a902df8c48622feec5e428f739709a532fe7
Nina Bartnik <259290@student.pwr.edu.pl>
2023-04-27 21:08:10 +0200

    Add project files.

---
+++ NFilosophersProblem/Fork.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NFilosophersProblem
+{
+    internal class Fork
+    {
+        bool isPicked = false; //na poczatku polozony
+
+        public void acquire()
+        {
+
+        }   
+
+        public void release()
+        {
+            this.isPicked = false;
+        }
+
+    }
+
+    internal class Philosopher { 
+        int ID { get; set; }
+        bool leftFork = false;  
+        bool rightFork = false;
+        bool forks = false;
+    
+    }
+
+}
---
+++ NFilosophersProblem/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NFilosophersProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter the number of philosophers");
+
+            int n = int.Parse(Console.ReadLine());
+        }
+    }
+}
